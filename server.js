3dc694c7f297af2288e08ce475c4216690338edc
Scottie <scott.c.houghton@gmail.com>
2022-06-28 20:34:03 -0700

    Added routes in the back end server so the front end can post/create new Books in the database and delete Books from the database.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const Book = require('./models/books.js');
+const { response } = require('express');
 
 mongoose.connect(process.env.DB_URL);
 
@@ -18,6 +19,7 @@ db.once('open', function () {
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 
@@ -26,11 +28,34 @@ app.get('/', (request, response) => {
 });
 
 app.get('/books', getBooks);
+app.post('/books', postBooks);
+app.delete('/books/:id', deleteBooks);
 
 async function getBooks(request, response, next) {
   try {
     let results = await Book.find();
     response.status(200).send(results);
+  } catch(error) {
+    next(error);
+  }
+}
+
+async function postBooks(request, response, next) {
+  console.log(request.body);
+  try {
+    let createdBook = await Book.create(request.body);
+    response.status(200).send(createdBook);
+  } catch(error) {
+    next(error);
+  }
+}
+
+async function deleteBooks(request, resonse, next) {
+  let id =request.params.id;
+  console.log(id);
+  try {
+    await Book.findByIdAndDelete(id);
+    response.status(200).send('Book deleted');
   } catch(error) {
     next(error);
   }
